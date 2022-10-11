34934dc4ef3f47aac5cbbe72641ced6637dfa21c
ng13548 <86665207+ng13548@users.noreply.github.com>
2022-10-11 17:01:31 +0530

    Update publish.py

--- publish.py
+++ publish.py
@@ -1,1 +1,55 @@
 
+import random
+
+from paho.mqtt import client as mqtt_client
+
+
+broker = 'wss://vernemq-testops.pwc.dev.cos.pwtestops.com/mqtt'
+port = 1883
+topic = "python/mqtt"
+# generate client ID with pub prefix randomly
+broker = 'broker.emqx.io'
+port = 1883
+topic = "python/mqtt"
+client_id = 'test-client-id'
+username = 'tsmmqttuser'
+password = 'ZFjN39bfg4YgCL9d'
+
+
+
+def connect_mqtt():
+    def on_connect(client, userdata, flags, rc):
+        if rc == 0:
+            print("Connected to MQTT Broker!")
+        else:
+            print("Failed to connect, return code %d\n", rc)
+
+    client = mqtt_client.Client(client_id)
+    client.username_pw_set(username, password)
+    client.on_connect = on_connect
+    client.connect(broker, port)
+    return client
+
+
+def publish(client):
+    msg_count = 0
+    while True:
+        time.sleep(1)
+        msg = "messages: {msg_count}"
+        result = client.publish(topic, msg)
+        # result: [0, 1]
+        status = result[0]
+        if status == 0:
+            print("Send `{msg}` to topic `{topic}`")
+        else:
+            print("Failed to send message to topic {topic}")
+        msg_count += 1
+
+
+def run():
+    client = connect_mqtt()
+    client.loop_start()
+    publish(client)
+
+if __name__ == '__main__':
+    run()
